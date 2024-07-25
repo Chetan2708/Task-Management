0424d70b10a213e4cd9fbbff396b469529cbc7a8
Chetan Gupta <chetangupta2708@gmail.com>
2024-07-26 02:11:47 +0530

    Started off with Google oauth

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,15 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { rateLimit } from "express-rate-limit";
-
+import passport from "./utils/passport.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173")
+  next()
+})
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -25,8 +32,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+console.log(process.env.CORS_ORIGIN)
 app.use(cookieParser());
+passport(app)
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -43,10 +51,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 // Routes Import
-
+import  authRoutes from "./routes/auth.routes.js"
 import taskRouter from "./routes/task.routes.js";
 
 // Routes Use
 app.use("/api/v1/task", taskRouter);
+app.use("/auth", authRoutes);
 
 export { app };
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,67 @@
+
+import mongoose from "mongoose"
+import bcrypt from "bcrypt" 
+import crypto from "crypto"
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [
+            true,
+            "Please add an email!"
+        ],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [
+            true,
+            "Please add a password!"
+        ]
+    },
+    pic:{
+        type: String,
+        default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+},
+    {
+        timestamps: true
+    }
+)
+
+
+// **** Methods for User Model ****/
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString("hex")
+  
+    this.passwordResetToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex")
+  
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000 
+  
+    return resetToken
+  }
+  
+
+
+export default mongoose.model("User", userSchema)
